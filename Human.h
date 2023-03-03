01fd5b5f5552a066b9de1499e503b740123813e0
Jack Hill <jackhill2104@gmail.com>
2023-03-03 13:33:09 +1030

    Boilerplate

---
+++ Human.h
@@ -0,0 +1,12 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+class Human {
+    private:
+        
+
+    public:
+
+}
+
+#endif
---
+++ Referee.h
@@ -0,0 +1,12 @@
+#ifndef REFEREE_H
+#define REFEREE_H
+
+class Referee {
+    private:
+        
+
+    public:
+
+}
+
+#endif
