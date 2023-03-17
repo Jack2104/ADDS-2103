9da1f4b84449a16c4fd5152d45e2836248a2784b
Jack Hill <jackhill2104@gmail.com>
2023-03-17 13:49:49 +1030

    misc

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,8 +1,12 @@
 #include "Truckloads.h"
 
+#include <cmath>
+
 int Truckloads::numTrucks(int numCrates, int loadSize) {
     // Base case
     if (numCrates <= loadSize) {
-        
+        return 1;
     }
+
+    return numTrucks(std::floor(numCrates / 2.0f), loadSize) + numTrucks(std::ceil(numCrates / 2.0f), loadSize);
 }
