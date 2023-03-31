65be583ae06a3ace4a438734db0d7fd7318c8f12
Jack Hill <jackhill2104@gmail.com>
2023-03-31 18:53:35 +1030

    revert

--- Finder.cpp
+++ Finder.cpp
@@ -6,11 +6,10 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    int prev_found = 0;
 
     for(size_t i = 1; i <= s2.size(); i++) {
         // The index that the previous prefix was found at
-        // int prev_found = i > 1 ? result[i - 1] : 0;
+        int prev_found = i > 1 ? result[i - 1] : 0;
 
         // Start the search at prev_found, as current prefix won't
         // be found before the index of the previous prefix
@@ -18,7 +17,6 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
 
         if (found != string::npos) {
             result.push_back(found);
-            prev_found++;
         } else {
             result.insert(result.end(), s2.size() - i + 1, -1);
             return result;
