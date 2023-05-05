09d090ba91da53c3a4a5a6c906dc5d4aba609f86
= <jackhill21@icloud.com>
2023-05-05 13:41:55 +0930

    misc

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -35,9 +35,31 @@ void LinkedList::printList() {
 /////////// your code goes here... DO NOT change the function signatures ///////////
 
 bool LinkedList::swap(int pos1, int pos2) {
-
+	
 }
 
 bool LinkedList::find_and_delete(int target) {
+	// Instantly return false if the list is empty. Prevents error from occuring if head = nullptr (when creating next_node).
+	if (head == nullptr) {
+		return false;
+	}
 
+	Node* current_node = head;
+	Node* prev_node = head;
+	Node* next_node = head->link;
+
+	while (current_node != nullptr) {
+		if (current_node->data == target) {
+			prev_node->link = next_node;
+			delete current_node;
+
+			return false;
+		}
+
+		prev_node = current_node;
+		current_node = current_node->link;
+		next_node = current_node->link;
+	}
+
+	return false;
 }
