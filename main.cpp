82164fcf78f8cd5ccaecb582f5b7157d58365305
Jack Hill <jackhill2104@gmail.com>
2023-05-22 12:23:27 +0930

    misc

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include "Heap.tpp"
-#include "klargest.cpp"
 #include <vector>
-#include <iostream>
 
 int main() {
     std::vector<int> v = {1, 2, 3, 7, 5, 6, 10, 9, 8};
@@ -9,7 +7,4 @@ int main() {
 
     h->insert(0);
     h->remove(3);
-
-    std::cout << kth_largest(v, 1) << std::endl;
-    return 0;
 }
