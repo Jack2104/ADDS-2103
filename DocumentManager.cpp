37ee5022c98ee91cf5124909b3b816869cfef80b
Jack Hill <jackhill2104@gmail.com>
2023-05-27 01:29:20 +0930

    misc

---
+++ DocumentManager.cpp
@@ -0,0 +1,21 @@
+#include "DocumentManager.h"
+
+void DocumentManager::addDocument(string name, int id, int license_limit) {
+
+}
+
+void DocumentManager::addPatron(int patronID) {
+
+}
+
+int DocumentManager::search(string name) {
+    return 0;
+}
+
+bool DocumentManager::borrowDocument(int docid, int patronID) {
+    return false;
+}
+
+void DocumentManager::returnDocument(int docid, int patronID) {
+
+}
